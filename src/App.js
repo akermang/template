1a63c84c081ae9c75dc5d35c3c1eb313777799df
galakerman <akermang@gmail.com>
2018-12-30 18:20:06 +0200

    Helloworkd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import GalComponent from './componets/gal.component.jsx'
+import GalComponent from './componets/gal.component.jsx';
+import HelloWorld from './componets/helloWorld.component.jsx'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+       <HelloWorld name="Gal A"/>
+      
        <GalComponent/>
       </div>
     );
