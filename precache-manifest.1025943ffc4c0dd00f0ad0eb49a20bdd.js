288d45a942299ee4c84e52dddab9d88df434dd25
galakerman <akermang@gmail.com>
2019-03-20 02:04:38 +0200

    Updates

---
+++ precache-manifest.1025943ffc4c0dd00f0ad0eb49a20bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ae75d3d0d031fa40b15f",
+    "url": "/app-test/static/js/runtime~main.7af4987c.js"
+  },
+  {
+    "revision": "7491904a3818e3574414",
+    "url": "/app-test/static/js/main.9f799a55.chunk.js"
+  },
+  {
+    "revision": "2fd47238a5728e1c4a83",
+    "url": "/app-test/static/js/2.764f1db2.chunk.js"
+  },
+  {
+    "revision": "7491904a3818e3574414",
+    "url": "/app-test/static/css/main.640ec30d.chunk.css"
+  },
+  {
+    "revision": "4860ce17ce435c617b965b26ffea3777",
+    "url": "/app-test/index.html"
+  }
+];
